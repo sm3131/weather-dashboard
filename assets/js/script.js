f182dae1fea798e37d502c7dca3d0a87444a2202
sm3131 <sm2683@nau.edu>
2021-12-04 18:21:14 -0700

    created js variables to grab user input and use it in other functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,45 @@
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
-    var cityName = $("#city-name").val().trim();
-    console.log(cityName);
+    var city = $("#city").val().trim();
+    var state = $("#state").val().trim();
+    var country = $("#country").val().trim();
+    console.log(city);
+    console.log(state);
+    console.log(country);
 
-    
-    $(".current-city").text(cityName);
+    if(city && state && country) {
 
+        getCityWeather(city, state, country);
 
+        //clear old content
+        $("#city").val("");
+        $("#state").val("");
+        $("#country").val("");
+    } 
+    else {
+        alert("please enter a valid location");
+    }
+};
+  
+var getCityWeather = function(city) {
+    var apiUrl = 
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+                displayWeather(data, city);
+            })
+        }
+    })
 }
+
+
+// var displayWeather = function (weather, citySearch) {
+
+// }
+
 
 $(".city-form").on("submit", formSubmitHandler); 
